db5142b5f4345ba7daf9f5d757de46410988e83c
minahkim03 <minahkim026@gmail.com>
2024-09-26 21:11:05 +0900

    프로젝트 세팅

---
+++ src/main/java/com/goorm/goormweek2/GoormWeek2Application.java
@@ -0,0 +1,13 @@
+package com.goorm.goormweek2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoormWeek2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoormWeek2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/goorm/goormweek2/GoormWeek2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goorm.goormweek2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoormWeek2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
